0ee66829d057117ed269490798bcf4c86f99ec9f
Mosesbwire <mosesdev92@gmail.com>
2023-08-19 15:54:55 +0000

    Add portfolio model , add unittests

---
+++ models/portfolio.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    Module: portfolio
+    Portfolio class
+"""
+
+
+from models.base_model import BaseModel
+
+class Portfolio(BaseModel):
+    """ 
+        Class represents a users portfolio
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        """ class constructor """
+        super().__init__()
+
+        if args:
+            self.name = args[0]
+            self.capital = args[1]
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
