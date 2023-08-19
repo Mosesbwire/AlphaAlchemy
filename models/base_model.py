afd3cadb54a06ab1e30cb9f10edc1cd201172614
Mosesbwire <mosesdev92@gmail.com>
2023-08-19 09:19:06 +0000

    add unittests for base model class. Build out BaseModel class (tdd)

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+    Module: base_model
+    Contains the BaseModel class
+"""
+
+from datetime import datetime, timezone
+import uuid
+
+
+class BaseModel:
+    """ BaseModel class which provides a blueprint that other models will inherit from """
+
+    def __init__(self):
+        """ constructor function """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now(timezone.utc)
+        self.updated_at = datetime.now(timezone.utc)
+
+    def __str__(self):
+        """ returns string representation of the object when printed """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def to_dict(self):
+        """ converts instance attributes to a dict """
+        date_format = "%Y-%m-%d %H:%M:%S.%f"
+        instance_dict = self.__dict__
+        
+        created_at = self.created_at
+        updated_at = self.updated_at
+        
+        instance_dict["created_at"] = created_at.strftime(date_format)
+        
+        instance_dict["updated_at"] = updated_at.strftime(date_format)
+
+        return instance_dict
+
+
