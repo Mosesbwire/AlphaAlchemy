f7d6e4ae06a0a362768bca1842457490a12c131d
musa1992 <mosesdev92@gmail.com>
2023-08-28 23:45:14 +0300

    test login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import SignUp from './pages/SignUpPage/SignUp'
+import Login from './pages/LoginPage/Login'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <SignUp/>
+      <Login/>
     </>
   )
 }
