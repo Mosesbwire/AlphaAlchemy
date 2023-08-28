fc58097df59742116c7f78d1b297a174bc19b814
musa1992 <mosesdev92@gmail.com>
2023-08-29 02:45:16 +0300

    Separate Form.jsx into two separate components - LoginForm & SignUpForm

---
+++ client/src/components/AuthForm/LoginForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Button from '../AuthBtn/Button'
+import useForm from '../../hooks/useForm'
+import './Form.css'
+
+const inputs = [
+    {name: 'email', type: 'email', placeholder: 'Email Address'},
+    {name: 'password', type: 'password', placeholder: 'Password'},
+]
+
+function LoginForm(){
+    const initialValues = {email: "", password:""}
+    const [values, handleChange, resetForm] = useForm(initialValues)
+
+    const onSubmit = async(event) =>{
+        event.preventDefault()
+        console.log(values)
+        resetForm()
+    }
+    return(
+        <form className='form' onSubmit={onSubmit}>
+            {inputs.map((input)=>(
+                <div key={input.name} className='input' id={input.name}>
+                    <input 
+                        type={input.type} 
+                        name={input.name} 
+                        placeholder={input.placeholder}
+                        value = {values[input.name]}
+                        onChange={e => handleChange(e)}
+                    />
+                </div>
+            ))}
+
+            <div className='btn'>
+                <Button text = {"Login"} />
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
