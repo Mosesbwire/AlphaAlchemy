3153b144be9bbde9e284d1bffd28fbda6d7fda04
musa1992 <mosesdev92@gmail.com>
2023-09-03 03:45:42 +0300

    modify Table component to be responsive

--- client/src/components/Table/TableRow.jsx
+++ client/src/components/Table/TableRow.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import './Table.css'
 
-const alpha = Array.from(Array(26)).map((e, i) => i + 65);
-const alphabet = alpha.map((x) => String.fromCharCode(x));
-
-const TableRow = ({data}) =>{
+const TableRow = ({data, classes}) =>{
     return (
         <div className="table-row">
             {data.map((dt, idx) => (
-                <div className={`${alphabet[idx]}-row`}>{dt}</div>
+                <div className={`${classes[idx]}`}>{dt}</div>
             ))}
         </div>
     )
