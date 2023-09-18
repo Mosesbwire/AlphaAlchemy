ec47a9e59bbee932294b61074a97b5ea59d4b0c4
Mosesbwire <mosesdev92@gmail.com>
2023-09-18 12:50:29 +0000

    create scheduling functionality

---
+++ scheduler/scheduler.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from services.daily_portfolio_value import DailyPortfolioValuationService
+from services.stock_data_service import StockDataService
+import schedule
+import time
+
+dataService = StockDataService()
+valuationService = DailyPortfolioValuationService()
+
+
+schedule.every().day.at("17:00").do(dataService.add_daily_stock_data)
+schedule.every().day.at("17:05").do(valuationService.scheduled_portfolio_valuation_job)
+
+
+if __name__ == "__main__":
+    while True:   
+        schedule.run_pending()
+        time.sleep(1)
+        
