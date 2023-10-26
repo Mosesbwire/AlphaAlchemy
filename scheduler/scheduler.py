3dc2252410d292cf11104997a5da67ab26161844
Mosesbwire <mosesdev92@gmail.com>
2023-10-26 11:41:56 +0300

    remove methods calls to delted class

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -6,16 +6,13 @@ import schedule
 import time
 
 dataService = StockDataService()
-valuationService = DailyPortfolioValuationService()
 
 
 schedule.every().day.at("17:00").do(dataService.add_daily_stock_data)
-schedule.every().day.at("17:05").do(valuationService.scheduled_portfolio_valuation_job)
 
 
 if __name__ == "__main__":
-    while True:   
-        print("Running")
+    while True:
+
         schedule.run_pending()
         time.sleep(1)
-        
