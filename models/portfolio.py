900e5acb407a853224689f7d322409f79fbfc882
Mosesbwire <mosesdev92@gmail.com>
2023-10-30 12:02:37 +0300

    Add function to calculate market value of portfolio

--- models/portfolio.py
+++ models/portfolio.py
@@ -5,9 +5,11 @@
     Portfolio class
 """
 
-import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, ForeignKey, String
+import models
+from models.portfolio_stock import PortfolioStock
+from models.stock import Stock
+from sqlalchemy import Column, ForeignKey, String, select
 from sqlalchemy.orm import relationship
 
 
@@ -31,3 +33,17 @@ class Portfolio(BaseModel, Base):
     def create(self, user):
         user.portfolios.append(self)
         return self
+
+    def portfolio_stocks_valuation(self, stock_data):
+        """calculates the current market value of stocks in the portfolio"""
+
+        statement = select(PortfolioStock.quantity, Stock.ticker).join(
+            PortfolioStock.stock).where(PortfolioStock.portfolio_id == self.id)
+        valuation = 0
+        data = models.storage.execute(statement)
+        for st_data in stock_data:
+            ticker = st_data.get("ticker")
+            for dt in data:
+                if ticker == dt.get("ticker"):
+                    valuation += (dt.get("quantity") * st_data.get("price"))
+        return valuation
