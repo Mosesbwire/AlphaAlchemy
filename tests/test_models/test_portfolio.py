fd8b8d4a18105d1f3438af0d71e30b7dce3415f3
Mosesbwire <mosesdev92@gmail.com>
2023-10-30 12:03:13 +0300

    Write unittest to test portfolio_stocks_valuation function

--- tests/test_models/test_portfolio.py
+++ tests/test_models/test_portfolio.py
@@ -15,9 +15,23 @@ class TestPortfolio(unittest.TestCase):
 
     """ test class for the stock data class """
 
-    def test_create_function_appends_portfolio_to_user(self):
-        portfolio = Portfolio()
-        user = Mock()
-
-    def test_valuation_is_calculated_correctly(self):
+    @patch("models.portfolio.models")
+    def test_valuation_is_calculated_correctly(self, mock_db):
         """test that valuation is calculated using recent data"""
+
+        mock_db.storage.execute.return_value = [
+            {"ticker": "SCOM", "quantity": 200},
+            {"ticker": "EQTY", "quantity": 200}
+        ]
+
+        stock_data = [
+            {"ticker": "SCOM", "price": 13.00},
+            {"ticker": "EQTY", "price": 37.95}
+        ]
+        portfolio = Portfolio()
+
+        expected_valuation = 10190.0
+        actual_valuation = portfolio.portfolio_stocks_valuation(stock_data)
+        mock_db.stop()
+        self.assertEqual(expected_valuation, actual_valuation)
+        mock_db.storage.execute.assert_called_once()
