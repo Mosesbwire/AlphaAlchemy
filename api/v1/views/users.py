0215e9a23dca20161c7c57fb96e2759704e1a986
Mosesbwire <mosesdev92@gmail.com>
2023-11-02 16:34:19 +0300

    use controller in route

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,44 +3,24 @@
 from api.v1.views import app_views
 from api.v1.error import BadRequest
 from api.v1.views.auth import is_authenticated
-from flask import abort, jsonify, make_response, request
+from flask import abort, jsonify, make_response, request, Response
 import jwt
 import os
 from services.user_service import UserService
 from services.stock_service import StockService
+from controllers.users import UserController
 
 service = UserService()
 stockService = StockService()
 
 
-@app_views.route("/users", methods = ["POST"])
+@app_views.route("/users", methods=["POST"])
 def create_user():
-    data = request.get_json()
-    
-    if not data:
-        raise BadRequest("Request Body is empty")
-
-    first_name = data.get("first_name", "")
-    last_name = data.get("last_name", "")
-    email = data.get("email", "")
-    password = data.get("password", "")
-    confirm_password = data.get("confirm_password", "")
-
-    user_data = service.create(first_name, last_name, email, password, confirm_password)
-
-    if user_data.get("error"):
-        raise BadRequest(user_data.get("error"))
-    
-    user = user_data.get("user")
-    token = jwt.encode({
-            "user": {"id": user.id}
-        },os.getenv("SECRET_KEY"), algorithm="HS256")
+    res = UserController.create(request)
+    return res
 
 
-    return make_response(jsonify({"token": token }), 201)
-
-
-@app_views.route("/users", methods = ["GET"])
+@app_views.route("/users", methods=["GET"])
 @is_authenticated
 def get_users():
     users_list = []
@@ -48,8 +28,9 @@ def get_users():
 
     for user in users:
         users_list.append(user.to_dict())
-    
+
     return jsonify(users_list)
+
 
 @app_views.route("/users/<user_id>", methods=["GET"])
 @is_authenticated
@@ -60,6 +41,7 @@ def get_user(user_id):
     if user is None:
         abort(404)
     return jsonify(user.to_dict())
+
 
 @app_views.route("/users/<user_id>", methods=["PUT"])
 @is_authenticated
