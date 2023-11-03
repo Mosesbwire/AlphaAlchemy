3a94578258fdb675922daef42e2df320bd90b504
Mosesbwire <mosesdev92@gmail.com>
2023-11-03 18:09:58 +0300

    Use portfolio controller action create in route to create a portfolio

--- api/v1/views/portfolio.py
+++ api/v1/views/portfolio.py
@@ -2,42 +2,24 @@
 
 from api.v1.views import app_views
 from api.v1.error import BadRequest
-from api.v1.views.auth import is_authenticated
+from controllers.portfolio import PortfolioController
 from flask import abort, jsonify, make_response, request
+from services.auth import Auth
 from services.portfolio_service import PortfolioService
 from services.user_service import UserService
 
 service = PortfolioService()
 userService = UserService()
 
-@app_views.route("/portfolios", methods = ["POST"])
-@is_authenticated
-def create_portfolio(user_id):
-    
-    data = request.get_json()
 
-    if not data:
-        raise BadRequest("Request body is Empty")
+@app_views.route("/portfolios", methods=["POST"])
+@Auth.is_authenticated
+def create_portfolio():
+    resp = PortfolioController.create(request)
+    return resp
 
-    name = data.get("name")
-    
-    user = userService.get_user_by_id(user_id)
-    
-    if user is None:
-        abort(404)
 
-    portfolio_data = service.create(user.id, name)
-
-    if portfolio_data.get("error"):
-        raise BadRequest(portfolio_data.get("error"))
-
-    portfolio = portfolio_data.get("portfolio")
-    portfolio = portfolio.to_dict()
-    del portfolio["user"]
-    return make_response(jsonify(portfolio))
-
-@app_views.route("/portfolios/<id>", methods = ["GET"])
-@is_authenticated
+@app_views.route("/portfolios/<id>", methods=["GET"])
 def get_portfolio_by_id(id):
     portfolio = service.get_portfolio_details(id)
     if portfolio is None:
@@ -45,16 +27,15 @@ def get_portfolio_by_id(id):
 
     return make_response(jsonify(portfolio))
 
-@app_views.route("/portfolios", methods = ["GET"])
-@is_authenticated
+
+@app_views.route("/portfolios", methods=["GET"])
 def get_user_portfolios(user_id):
     portfolios = service.get_user_portfolios(user_id)
 
     return make_response(jsonify(portfolios))
 
-@app_views.route("/portfolios/<portfolio_id>/buy", methods = ["POST"])
-@is_authenticated
 
+@app_views.route("/portfolios/<portfolio_id>/buy", methods=["POST"])
 def buy_stocks(portfolio_id, user_id):
     data = request.get_json()
 
@@ -64,20 +45,20 @@ def buy_stocks(portfolio_id, user_id):
     quantity = data.get("quantity", None)
     bid_price = data.get("price", None)
 
-    transaction = service.buy_action(user_id, portfolio_id, stock_id, float(quantity), float(bid_price))
+    transaction = service.buy_action(
+        user_id, portfolio_id, stock_id, float(quantity), float(bid_price))
     error = transaction.get("error")
 
     if error:
         raise BadRequest(error)
-    
+
     data = transaction.get("transaction")
     data = data.to_dict()
     del data["portfolio"]
     return make_response(jsonify(data))
 
-@app_views.route("/portfolios/<portfolio_id>/sell", methods = ["POST"])
-@is_authenticated
 
+@app_views.route("/portfolios/<portfolio_id>/sell", methods=["POST"])
 def sell_stock(portfolio_id, user_id):
     data = request.get_json()
 
@@ -88,7 +69,8 @@ def sell_stock(portfolio_id, user_id):
     quantity = data.get("quantity", None)
     ask_price = data.get("price", None)
 
-    transaction = service.sell_action(user_id, portfolio_id, stock_id, float(quantity), float(ask_price))
+    transaction = service.sell_action(
+        user_id, portfolio_id, stock_id, float(quantity), float(ask_price))
 
     error = transaction.get("error")
 
@@ -101,11 +83,10 @@ def sell_stock(portfolio_id, user_id):
 
     return make_response(jsonify(data))
 
-@app_views.route("/portfolio/<portfolio_id>/transactions", methods = ["GET"])
-@is_authenticated
 
+@app_views.route("/portfolio/<portfolio_id>/transactions", methods=["GET"])
 def get_portfolio_transactions(portfolio_id):
-    
+
     transactions = []
     transaction_objects = service.get_portfolio_transactions(portfolio_id)
 
