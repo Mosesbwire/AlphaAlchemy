360e49be9a5c7efc5b4f6eae6760356bd4f905a7
Mosesbwire <mosesdev92@gmail.com>
2023-11-05 23:45:02 +0300

    use respective controller functions to the routes"

--- api/v1/views/portfolio.py
+++ api/v1/views/portfolio.py
@@ -19,78 +19,10 @@ def create_portfolio():
     return resp
 
 
-@app_views.route("/portfolios/<id>", methods=["GET"])
-def get_portfolio_by_id(id):
-    portfolio = service.get_portfolio_details(id)
-    if portfolio is None:
-        abort(404)
+@app_views.route("/portfolios/buy", methods=["POST"])
+@Auth.is_authenticated
+def buy():
+    user = Auth.logged_user(request)
+    resp = PortfolioController.buy_stock(user, request)
 
-    return make_response(jsonify(portfolio))
-
-
-@app_views.route("/portfolios", methods=["GET"])
-def get_user_portfolios(user_id):
-    portfolios = service.get_user_portfolios(user_id)
-
-    return make_response(jsonify(portfolios))
-
-
-@app_views.route("/portfolios/<portfolio_id>/buy", methods=["POST"])
-def buy_stocks(portfolio_id, user_id):
-    data = request.get_json()
-
-    if not data:
-        raise BadRequest("Request Body is empty")
-    stock_id = data.get("security", None)
-    quantity = data.get("quantity", None)
-    bid_price = data.get("price", None)
-
-    transaction = service.buy_action(
-        user_id, portfolio_id, stock_id, float(quantity), float(bid_price))
-    error = transaction.get("error")
-
-    if error:
-        raise BadRequest(error)
-
-    data = transaction.get("transaction")
-    data = data.to_dict()
-    del data["portfolio"]
-    return make_response(jsonify(data))
-
-
-@app_views.route("/portfolios/<portfolio_id>/sell", methods=["POST"])
-def sell_stock(portfolio_id, user_id):
-    data = request.get_json()
-
-    if not data:
-        raise BadRequest("Request body is empty")
-
-    stock_id = data.get("security", None)
-    quantity = data.get("quantity", None)
-    ask_price = data.get("price", None)
-
-    transaction = service.sell_action(
-        user_id, portfolio_id, stock_id, float(quantity), float(ask_price))
-
-    error = transaction.get("error")
-
-    if error:
-        raise BadRequest(error)
-
-    data = transaction.get("transaction")
-    data = data.to_dict()
-    del data["portfolio"]
-
-    return make_response(jsonify(data))
-
-
-@app_views.route("/portfolio/<portfolio_id>/transactions", methods=["GET"])
-def get_portfolio_transactions(portfolio_id):
-
-    transactions = []
-    transaction_objects = service.get_portfolio_transactions(portfolio_id)
-
-    for transaction in transaction_objects:
-        transactions.append(transaction.to_dict())
-
-    return make_response(jsonify(transactions))
+    return resp
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,60 +3,20 @@
 from api.v1.views import app_views
 from api.v1.error import BadRequest
 
-from flask import abort, jsonify, make_response, request, Response
-import jwt
-import os
-from services.user_service import UserService
-from services.stock_service import StockService
+from flask import request
 from services.auth import Auth
 from controllers.users import UserController
 
-service = UserService()
-stockService = StockService()
 
-
-@app_views.route("/users", methods=["POST"])
+@app_views.route("/users", methods=["POST"])  # type: ignore
 def create_user():
     res = UserController.create(request)
     return res
 
 
-@app_views.route("/users", methods=["GET"])
+@app_views.route("/users/portfolio", methods=["GET"])
 @Auth.is_authenticated
-def get_users():
-    users_list = []
-    users = service.get_users()
+def get_portfolio():
 
-    for user in users:
-        users_list.append(user.to_dict())
-
-    return jsonify(users_list)
-
-
-@app_views.route("/users/<user_id>", methods=["GET"])
-@Auth.is_authenticated
-def get_user(user_id):
-
-    user = service.get_user_by_id(user_id)
-
-    if user is None:
-        abort(404)
-    return jsonify(user.to_dict())
-
-
-@app_views.route("/users/<user_id>", methods=["PUT"])
-@Auth.is_authenticated
-def update_user(user_id):
-    data = request.get_json()
-    if not data:
-        raise BadRequest("Request body is empty")
-
-
-@app_views.route("/users/<user_id>", methods=["DELETE"])
-@Auth.is_authenticated
-def delete_user(user_id):
-    resp = service.delete_user(user_id)
-
-    if resp is None:
-        abort(404)
-    return make_response(jsonify({"message": "User account successfully deleted."}))
+    res = UserController.get_portfolio(Auth.logged_user(request))
+    return res
